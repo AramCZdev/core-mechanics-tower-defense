607336c906897c53ae18958127f3af85638f19e1
AramCZ <aramcz@protonmail.com>
2026-08-19 19:36:17 +0200

    Menu improvments

--- Scripts/Menus/main.gd
+++ Scripts/Menus/main.gd
@@ -1,5 +1,16 @@
 extends Control
 
+@onready var main_menu = $Menu
+@onready var play_menu = $Difficulty
+
+func _ready() -> void:
+	play_menu.visible = false
+	main_menu.visible = true
 
 func _on_play_pressed() -> void:
+	play_menu.visible = true
+	main_menu.visible = false
+
+
+func _on_normal_mode_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Gameplay/Game.tscn")
